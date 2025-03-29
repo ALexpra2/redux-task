855b28eab2e1f8c3870187efed8c26765c11dfaa
Alexpra2 <alexpra2@gmail.com>
2025-03-29 01:45:01 +0100

    ejercicio terminado

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,41 @@
+import { useSelector, useDispatch } from 'react-redux';
+import { addTask, removeTask } from './redux/todosSlice';
+import { useState } from 'react';
+
 const App = () => {
-  
+  const tasks = useSelector((state) => state.todos.tasks);
+  const dispatch = useDispatch();
+  const [task, setTask] = useState('');
+
+  const handleAddTask = () => {
+    if (task.trim() !== '') {
+      dispatch(addTask(task));
+      setTask('');
+    }
+  };
+
   return (
-    <></>
+    <div style={{ textAlign: 'center', padding: '20px' }}>
+      <h1>Lista de Tareas</h1>
+      <input
+        type="text"
+        value={task}
+        onChange={(e) => setTask(e.target.value)}
+        placeholder="Escribe una tarea..."
+      />
+      <button onClick={handleAddTask}>Agregar Tarea</button>
+
+      <ul>
+        {tasks.map((t, index) => (
+          <li key={index}>
+            {t}
+            <button onClick={() => dispatch(removeTask(index))} >
+              Eliminar
+            </button>
+          </li>
+        ))}
+      </ul>
+    </div>
   );
 };
 
